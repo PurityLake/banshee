ee8693d543d6968a4b6e16268a039be7b07a7fa8
Robert O'Shea <robertoshea2k11@gmail.com>
2023-07-11 18:42:36 +0100

    Updated build system

--- iso.sh
+++ iso.sh
@@ -6,10 +6,16 @@ mkdir -p isodir
 mkdir -p isodir/boot
 mkdir -p isodir/boot/grub
 
+if [ ! -z $(which grub2-mkrescue)]; then
+	MKRESCUE=grub2-mkrescue
+else
+	MKRESCUE=grub-mkrescue
+fi
+
 cp sysroot/boot/banshee.kernel isodir/boot/banshee.kernel
 cat > isodir/boot/grub/grub.cfg << EOF
 menuentry "banshee" {
 	multiboot /boot/banshee.kernel
 }
 EOF
-grub2-mkrescue -o banshee.iso isodir
+$MKRESCUE -o banshee.iso isodir
--- qemu.sh
+++ qemu.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 set -e
-. ./iso.sh
 
 qemu-system-$(./target-triplet-to-arch.sh $HOST) -cdrom banshee.iso \
   -serial file:serial.log
